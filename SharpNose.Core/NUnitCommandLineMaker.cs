a25e71c6c8d5240b744aa823768ae05c127a4e71
dhelper_cp <SND\dhelper_cp>
2009-11-09 16:36:21 +0000

    renamed projects and directories

---
+++ SharpNose.Core/NUnitCommandLineMaker.cs
@@ -0,0 +1,20 @@
+﻿namespace SharpNose.Core
+{
+    public class NUnitCommandLineMaker : CommandLineMaker
+    {
+        private string path;
+        public NUnitCommandLineMaker(string nunitPath) :base()
+        {
+            path = nunitPath;	
+        }
+		
+        override public string TestRunner
+        {
+            get
+            {
+                return path +"\\nunit-console.exe";
+            }
+			
+        }
+    }
+}
