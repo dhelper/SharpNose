a47b96544084a48858fb772a4e8980a86a4c37a2
dhelper_cp <SND\dhelper_cp>
2009-11-18 20:39:33 +0000

    Experimanting with UI

---
+++ SharpNose/ConfigForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SharpNose
+{
+	public partial class ConfigForm : Form
+	{
+		public ConfigForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
