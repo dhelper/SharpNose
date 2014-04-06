ea47fb53ee72aa4b01bac58f6305c4e430dcf029
Dror Helper <dhelper@users.noreply.github.com>
2014-04-06 18:35:11 +0000

    Chnages source binding

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyVersion("1.2.*")]
 [assembly: AssemblyProduct("SharpNose")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright 2009-2010")]
